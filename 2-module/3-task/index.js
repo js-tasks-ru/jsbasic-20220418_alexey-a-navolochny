a97f3c4c5e9c3c19cbd8a837a67635c7f2e3798a
alexey <navlalexey@mail.ru>
2022-05-10 23:28:58 +0400

    Выполнил 3 задание 2 модуля

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+	sum(){
+		return this.t + this.k;
+	},
+
+	mul(){
+		return this.t * this.k;
+	},
+	
+	read(a,b){
+	  this.t = a;
+	  this.k = b;
+	}
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
