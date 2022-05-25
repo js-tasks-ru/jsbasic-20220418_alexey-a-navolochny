159a983e2bfb7e9f662f62a6ffa193556dd382df
alexey navolochny <navlalexey@mail.ru>
2022-05-25 17:59:31 +0400

    Выполнил модуль 4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,3 @@
-function showSalary(users, age) {
-  // ваш код...
+function showSalary(users, year) {
+  return users.filter(user => user.age <= year).map(user => (`${user.name}, ${user.balance}`)).join('\n');
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,21 @@
 function getMinMax(str) {
-  // ваш код...
+  let row = str.split(' ');
+  
+	let result  = row.map(function(item, index, arr) {
+  		let number = parseInt(item);
+  		let val = isNaN(number)? item : number;
+		return val;
+    });
+    
+    let arr_mas = [];
+    for(let i = 0; i<result.length; i++){
+    	if(typeof(result[i]) == 'number'){
+    		arr_mas.push(result[i]);
+     	}
+    }
+    
+    let minVal = Math.min(...arr_mas);
+    let maxVal = Math.max(...arr_mas);
+	
+	return {min: minVal, max:maxVal}
 }
