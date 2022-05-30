ddd7bf4c8c76b33014e17e244635d5f1ad77568f
alexey navolochny <navlalexey@mail.ru>
2022-05-30 23:09:49 +0400

    Выполнил модуль 3 и 4

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,21 +1,12 @@
 function getMinMax(str) {
-  let row = str.split(' ');
-  
-	let result  = row.map(function(item, index, arr) {
-  		let number = parseInt(item);
-  		let val = isNaN(number)? item : number;
-		return val;
-    });
-    
-    let arr_mas = [];
-    for(let i = 0; i<result.length; i++){
-    	if(typeof(result[i]) == 'number'){
-    		arr_mas.push(result[i]);
-     	}
-    }
-    
-    let minVal = Math.min(...arr_mas);
-    let maxVal = Math.max(...arr_mas);
-	
-	return {min: minVal, max:maxVal}
+  let array = str.split(/[ ,]+/);
+ 
+  let minV = +array[0];
+  let maxV = minV;
+  for (let i = 0; i < array.length; i++) {
+    let e = +array[i];
+    if (e < minV) minV = e;
+    if (e > maxV) maxV = e;
+  }
+  return {min: minV, max: maxV};
 }
